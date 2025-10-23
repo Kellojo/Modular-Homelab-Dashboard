558dbae83181cc9adee1c76225360f2a7e051eb2
Kellojo <12833426+Kellojo@users.noreply.github.com>
2025-10-23 23:45:35 +0200

    Added gitea integration

--- src/routes/api/plugins/gitea/GiteaClient.ts
+++ src/routes/api/plugins/gitea/GiteaClient.ts
@@ -1,4 +1,6 @@
 import getConfig from '$lib/server/Config';
+import { env } from '$env/dynamic/private';
+import * as si from 'systeminformation';
 
 export default class GiteaClient {
 	constructor() {}
@@ -8,13 +10,95 @@ export default class GiteaClient {
 		return config.plugins.gitea?.url || null;
 	}
 
-	async getVersion(): Promise<string> {
+	async getApiUrl(path: string): Promise<string> {
 		const baseUrl = await this.getGiteaUrl();
-		if (!baseUrl) return 'N/A';
 
-		const response = await fetch(new URL('/api/v1/version', baseUrl).toString());
+		if (!baseUrl) throw new Error('Gitea URL is not configured');
+		const url = new URL(path, baseUrl);
+		return url.toString();
+	}
+
+	async getVersion(): Promise<string> {
+		const response = await fetch(await this.getApiUrl('/api/v1/version'));
 		const data = await response.json();
 
 		return data.version;
 	}
+
+	async getAllRunners(): Promise<GiteaRunner[]> {
+		const apiUrl = await this.getApiUrl('/api/v1/admin/actions/runners');
+		const response = await this.authenticatedFetch(apiUrl);
+		const data: GiteaRunnersResponse = await response.json();
+		return data.runners;
+	}
+
+	async getAllRepos(filters: { search: string }): Promise<Repository[]> {
+		const apiUrl = await this.getApiUrl('/api/v1/repos/search');
+		const url = new URL(apiUrl);
+		if (filters.search) {
+			url.searchParams.append('q', filters.search);
+		}
+
+		const response = await this.authenticatedFetch(url.toString());
+		const data: RepositorySearchResponse = await response.json();
+		return data.data;
+	}
+
+	async authenticatedFetch(url: string, options: RequestInit = {}): Promise<Response> {
+		const token = env.GITEA_TOKEN;
+		if (!token) throw new Error('GITEA_TOKEN is not set');
+
+		const headers = new Headers(options.headers);
+		headers.set('Authorization', `token ${token}`);
+
+		const response = await fetch(url, {
+			...options,
+			headers
+		});
+
+		if (response.status === 401) {
+			throw new Error('Unauthorized: Invalid GITEA_TOKEN');
+		}
+
+		return response;
+	}
+}
+
+interface GiteaRunnersResponse {
+	total_count: number;
+	runners: GiteaRunner[];
+}
+
+export interface GiteaRunner {
+	id: number;
+	name: string;
+	status: string;
+	busy: boolean;
+	ephemeral: boolean;
+	labels: GiteaRunnerLabel[];
+}
+
+export interface GiteaRunnerLabel {
+	id: number;
+	name: string;
+	type: string;
+}
+
+export interface Repository {
+	id: number;
+	owner: object;
+	name: string;
+	full_name: string;
+	description: string;
+	private: boolean;
+	fork: boolean;
+	template: boolean;
+	mirror: boolean;
+	size: number;
+	html_url: string;
+}
+
+export interface RepositorySearchResponse {
+	ok: boolean;
+	data: Repository[];
 }
---
+++ src/routes/api/plugins/gitea/admin/runners/busy/+server.ts
@@ -0,0 +1,44 @@
+import { ValueState } from '../../../../../../../lib/types/valueState';
+import { createWidgetEndpoint } from '$lib/server/StandardWidgetDataEndpoint';
+import GiteaClient, { type GiteaRunner } from '../../../GiteaClient';
+
+export const GET = createWidgetEndpoint('gitea/admin/runners/busy', async () => {
+	const giteaClient = new GiteaClient();
+	const runners = await giteaClient.getAllRunners();
+
+	let idleRunners = 0;
+	let busyRunners = 0;
+	runners.forEach((runner: GiteaRunner) => {
+		if (runner.busy) {
+			busyRunners++;
+		} else {
+			idleRunners++;
+		}
+	});
+
+	let text = `${busyRunners} busy, ${idleRunners} idle`;
+	if (busyRunners === 0 && idleRunners > 0) {
+		text = `${idleRunners} runner${idleRunners === 1 ? '' : 's'} idle`;
+	} else if (busyRunners > 0 && idleRunners === 0) {
+		text = `${busyRunners} runner${busyRunners === 1 ? '' : 's'} busy`;
+	} else if (runners.length === 0) {
+		text = 'No runners found';
+	}
+
+	let valueState = ValueState.Error;
+	if (busyRunners > 0) {
+		valueState = ValueState.Warning;
+	} else if (idleRunners > 0) {
+		valueState = ValueState.Success;
+	}
+
+	return {
+		value: runners.length,
+		classification: valueState,
+		unit: '',
+		displayValue: text,
+		min: 0,
+		max: runners.length,
+		url: await giteaClient.getGiteaUrl()
+	};
+});
