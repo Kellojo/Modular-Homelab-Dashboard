745b244b606e361a5a19b5ffc2ae1247a8dd9ab7
Kellojo <12833426+Kellojo@users.noreply.github.com>
2025-10-25 14:50:20 +0200

    Added image size widget

---
+++ src/routes/api/plugins/docker/images/size/+server.ts
@@ -0,0 +1,21 @@
+import { createWidgetEndpoint } from '$lib/server/StandardWidgetDataEndpoint';
+import type { FillDataWidgetValue } from '$lib/types/DataWidgetValueTypes';
+import { ValueState } from '$lib/types/valueState';
+import DockerClient from '../../DockerClient';
+
+export const GET = createWidgetEndpoint(
+	'docker/images/size',
+	async (): Promise<FillDataWidgetValue> => {
+		const dockerClient = new DockerClient();
+
+		const imageSize = await dockerClient.getOverallImageSize();
+
+		return {
+			value: imageSize,
+			classification: ValueState.Success,
+			unit: '',
+			displayValue: `${imageSize}`,
+			tooltip: `${new Date().toLocaleString()}: ${imageSize} images`
+		};
+	}
+);
