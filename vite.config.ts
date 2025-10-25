00d31817f757d40bfaf06c3c6e8fe41bb98969cb
Kellojo <12833426+Kellojo@users.noreply.github.com>
2025-10-26 00:56:10 +0200

    Fixed non working builds

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { execSync } from 'child_process';
 import { defineConfig } from 'vite';
 
-const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+//const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
-		__COMMIT_HASH__: JSON.stringify(commitHash)
+		__COMMIT_HASH__: 'TBD' //JSON.stringify(commitHash)
 	}
 });
